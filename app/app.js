b4f8b9f8cf0a7e0b7a0c6f21233de7cd8654fe92
Igor Randjelovic <rigor789@gmail.com>
2021-09-13 17:15:56 +0200

    chore: add test code

--- app/app.js
+++ app/app.js
@@ -1,7 +1,10 @@
-import Vue from 'nativescript-vue'
+import Vue from "nativescript-vue";
+import RadListView from "nativescript-ui-listview/vue";
 
-import Home from './components/Home'
+import Home from "./components/Home";
+
+Vue.use(RadListView);
 
 new Vue({
-  render: (h) => h('frame', [h(Home)]),
-}).$start()
+  render: h => h("frame", [h(Home)])
+}).$start();
--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,41 +1,97 @@
 <template>
     <Page>
         <ActionBar>
-            <Label text="Home"/>
+            <Label text="Home" />
         </ActionBar>
 
-        <GridLayout>
+        <GridLayout rows="auto, *, *">
             <Label class="info">
                 <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
+                    <Span class="fas" text.decode="&#xf135; " />
+                    <Span :text="message" />
                 </FormattedString>
             </Label>
+
+            <ListView for="item in items" row="1">
+                <v-template>
+                    <Label :text="item.text" />
+                </v-template>
+            </ListView>
+
+            <RadListView
+                scrollDirection="vertical"
+                pullToRefresh="true"
+                loadOnDemandMode="Auto"
+                loadOnDemandBufferSize="10"
+                @pullToRefreshInitiated="onRefresh"
+                @loadMoreDataRequested="onLoadMoreItems"
+                @itemTap="onItemTap"
+                for="item in items"
+                row="2"
+            >
+                <v-template>
+                    <Label :text="item.text" />
+                </v-template>
+            </RadListView>
         </GridLayout>
     </Page>
 </template>
 
 <script>
-  export default {
+const items = [];
+
+let i = 1;
+for (; i <= 100; i++) {
+    items.push({
+        text: `Item #${i}`
+    });
+}
+export default {
+    data() {
+        return {
+            items
+        };
+    },
     computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
+        message() {
+            return "Blank {N}-Vue app";
+        }
+    },
+    methods: {
+        onRefresh(args) {
+            setTimeout(() => {
+                this.items.unshift({
+                    text: `Prepended item #${i++}`
+                })
+                args.object.notifyPullToRefreshFinished()
+            }, 1000)
+        },
+        onLoadMoreItems(args) {
+            setTimeout(() => {
+                for (let j = 0; j < 10; j++) {
+                    items.push({
+                        text: `Appended item #${i++}`
+                    });
+                }
+                args.object.notifyLoadOnDemandFinished()
+            }, 2000)
+        },
+        onItemTap(args) {
+            console.log('tapped', args.item)
+        }
     }
-  };
+};
 </script>
 
 <style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
+// Custom styles
+.fas {
+    color: #65adf1;
+}
 
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
-
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+.info {
+    font-size: 20;
+    horizontal-align: center;
+    vertical-align: center;
+}
 </style>
